0801c0f9f98469c0858deb77cc4f1c6dacbbf24c
syedasif <asifsyed799549@gmail.com>
2025-12-10 00:06:08 +0530

    Switched from SQLite to PostgreSQL

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,10 +1,14 @@
 const { Sequelize } = require("sequelize");
-const path = require("path");
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: path.join(__dirname, "../database.sqlite"),
-  logging: false,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
